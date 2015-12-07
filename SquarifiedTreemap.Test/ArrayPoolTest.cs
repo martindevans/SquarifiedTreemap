38892fc9c0b2ef946cb72394a475ca66408f0880
Martin Evans <martindevans@gmail.com>
2015-12-07 04:29:48 +0000

    Implemented array pool
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ SquarifiedTreemap.Test/ArrayPoolTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SquarifiedTreemap.Test
+{
+    [TestClass]
+    public class ArrayPoolTest
+    {
+        private readonly ArrayPool<int> _pool = new ArrayPool<int>();
+
+        [TestMethod]
+        public void AssertThat_AllocatedArray_IsLargeEnough_WhenAllocatingNew()
+        {
+            Assert.IsTrue(_pool.Allocate(10).Length >= 10);
+        }
+
+        [TestMethod]
+        public void AssertThat_AllocatedArray_IsLargeEnough_WhenRecycling()
+        {
+            //Allocate a large enough array
+            var a = _pool.Allocate(10);
+
+            //Free it
+            _pool.Free(a);
+
+            //Allocate again, let's see if we get a large enough array
+            Assert.IsTrue(_pool.Allocate(5).Length >= 5);
+        }
+
+        [TestMethod]
+        public void AssertThat_FreeingArrayIntoFullPool_DoesNotThrow()
+        {
+            //Free items into pool, exceeding it's size and thus freeing into a full pool
+            Random r = new Random(345);
+            for (var i = 0; i < 100; i++)
+                _pool.Free(new int[r.Next(i) + 1]);
+        }
+    }
+}
--- SquarifiedTreemap/ArrayPool.cs
+++ SquarifiedTreemap/ArrayPool.cs
@@ -1,14 +1,65 @@
 ﻿namespace SquarifiedTreemap
 {
+    /// <summary>
+    /// A small pool of arrays, this save constantly allocating and disposing arrays for scratch space.
+    /// A future implementation of this could allocate an array up front and return slices
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     internal class ArrayPool<T>
     {
+        private int _count;
+        private readonly T[][] _pool = new T[16][];
+
         public T[] Allocate(int count)
         {
+            if (count > 0)
+            {
+                //Find the first large enough non-null item and return it
+                for (var i = 0; i < _pool.Length; i++)
+                {
+                    if (_pool[i] != null && _pool[i].Length >= count)
+                    {
+                        var arr = _pool[i];
+
+                        //Remove item from pool
+                        _pool[i] = null;
+                        _count--;
+
+                        return arr;
+                    }
+                }
+            }
+
             return new T[count];
         }
 
         public void Free(T[] arr)
         {
+            if (_count == _pool.Length)
+            {
+                //pool is full, try to replace a smaller item in the pool
+                for (var i = 0; i < _pool.Length; i++)
+                {
+                    if (_pool[i].Length < arr.Length)
+                    {
+                        _pool[i] = arr;
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                //Pool is not full, replace first null item
+                for (var i = 0; i < _pool.Length; i++)
+                {
+                    if (_pool[i]  == null)
+                    {
+                        _pool[i] = arr;
+                        _count++;
+                        return;
+                    }
+                }
+            }
         }
     }
 }
