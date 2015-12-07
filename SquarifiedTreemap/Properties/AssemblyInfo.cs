2070269c5d26c8c02637a74d0fae175adc2c5c06
Martin Evans <martindevans@gmail.com>
2015-12-07 04:33:02 +0000

    Added nuget stuff, pushed v1 to nuget
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- SquarifiedTreemap/Properties/AssemblyInfo.cs
+++ SquarifiedTreemap/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SquarifiedTreemap")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generates squarified treemap diagrams")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SquarifiedTreemap")]
---
+++ push-all.ps1
@@ -0,0 +1,24 @@
+function Push-Nuget($path, $csproj) {
+    $fullPathToCsprog = Join-Path -Path $path -ChildPath $csproj -resolve;
+    
+    nuget pack $fullPathToCsprog -Prop Configuration=Release -IncludeReferencedProjects
+    
+    get-childitem -Filter *.nupkg -name | foreach ($_) {
+        Write-Host "Pushing " $_ -backgroundcolor darkgreen -foregroundcolor white;
+    
+        nuget push $_
+        Remove-Item $_
+        
+        Write-Host "Done " $_ -backgroundcolor darkgreen -foregroundcolor white;
+    }
+}
+
+Push-Nuget "SquarifiedTreemap" "SquarifiedTreemap.csproj"
+
+#Delete all *.nupkg and *.symbols.nupkg
+get-childitem -Filter *.nupkg -name | foreach ($_) {
+    Remove-Item $_
+}
+get-childitem -Filter *.symbols.nupkg -name | foreach ($_) {
+    Remove-Item $_
+}
